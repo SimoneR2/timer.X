e661d934e2afed6c75bbae6c00ca9ed1d0bb9540
Simone <simone.righetti97@gmail.com>
2016-04-24 13:43:49 +0200

    Risolti errori vari

--- timer_main.c
+++ timer_main.c
@@ -1,7 +1,10 @@
 #include <xc.h>
 #include "timer.h"
 #define  _XTAL_FREQ 16000000
+#define buzzer PORTCbits.RC6
+#define segnale PORTAbits.RA0
 void configurazione(void);
+void delay(unsigned char ritardo);
 volatile signed char secondi, minuti = 0;
 unsigned char numeri [] = {
     0b11001111,
@@ -23,7 +26,7 @@ unsigned char porte [] = {
 };
 
 __interrupt(high_priority) void ISR_alta(void) {
-    if (INTCONbits.INT0IF == 1) {
+    if ((INTCONbits.INT0IF == 1)&&(INTCONbits.TMR0IF == 0)) {
         if (PORTBbits.RB1 == 0) {
             secondi++;
             if (secondi > 59) {
@@ -32,20 +35,23 @@ __interrupt(high_priority) void ISR_alta(void) {
             }
         } else {
             secondi--;
-            if (secondi < 1) {
+
+            if ((secondi < 0)&&(minuti > 0)) {
                 minuti--;
                 secondi = 59;
             }
         }
-
         INTCONbits.INT0IF = 0;
     }
+    
     if (INTCONbits.TMR0IF == 1) {
         secondi--;
         if (secondi < 0) {
+
             minuti--;
             secondi = 59;
         }
+        segnale = ~segnale;
         TMR0H = 0x0B;
         TMR0L = 0xDC;
         INTCONbits.TMR0IF = 0;
@@ -56,6 +62,14 @@ unsigned char disp[4] = 0;
 
 void main(void) {
     configurazione();
+    minuti = eeprom_read(0xE5);
+    secondi = eeprom_read(0xE6);
+    buzzer = 1;
+    for (unsigned char i = 0; i < 200; i++) {
+        __delay_ms(10);
+    }
+    buzzer = 0;
+
     while (1) {
         disp[3] = secondi / 10;
         disp[2] = secondi - (disp[3] * 10);
@@ -64,13 +78,15 @@ void main(void) {
 
         for (unsigned char i = 0; i < 4; i++) {
             PORTD = numeri[disp[i]];
-            if (i == 2){
-                PORTDbits.RD4 =1;
+            if (i == 4) {
+                PORTDbits.RD4 = 1;
             }
             PORTC = porte[i];
             __delay_ms(2);
         }
         if ((PORTBbits.RB2 == 0)&&((secondi != 0) || (minuti != 0))) {
+            eeprom_write(0xE5, minuti);
+            eeprom_write(0xE6, secondi);
             PORTAbits.RA5 = 1;
             INTCONbits.INT0IE = 0; //disabilito possibilità di modificare il tempo
             INTCONbits.TMR0IF = 0;
@@ -82,26 +98,31 @@ void main(void) {
         if ((T0CONbits.TMR0ON == 1)&&(secondi == 0)&(minuti == 0)) {
             T0CONbits.TMR0ON = 0;
             PORTAbits.RA5 = 0;
-            for (unsigned char c = 0; c<10; c++){
-            for (unsigned char i = 0; i < 4; i++) {
-                PORTD = numeri[0];
-                PORTC = porte[i];
-                __delay_ms(2);
-            }
-            for (unsigned char b = 0; b < 10; b++) {
-                __delay_ms(10);
-            }
-            for (unsigned char i = 0; i < 4; i++) {
-                PORTD = 0x00;
-                PORTC = porte[i];
-                __delay_ms(2);
-            }
-     
-            for (unsigned char f = 0; f < 10; f++) {
-                __delay_ms(10);
+            for (unsigned char c = 0; c < 10; c++) {
+                for (unsigned char s = 0; s < 100; s++) {
+                    for (unsigned char i = 0; i < 4; i++) {
+                        PORTD = numeri[0];
+                        PORTC = porte[i];
+                        __delay_ms(2);
+                    }
+                }
+                for (unsigned char i = 0; i < 4; i++) {
+                    PORTD = 0x00;
+                    PORTC = porte[i];
+                    __delay_ms(2);
+                }
+                delay(100);
+                INTCONbits.INT0IE = 1; //riabilito i tempi
             }
         }
-        }
+    }
+}
+
+void delay(unsigned char ritardo) {
+    ritardo = ritardo / 10;
+    while (ritardo > 0) {
+        __delay_ms(10);
+        ritardo--;
     }
 }
 
